25a585879e5bcea800b1e8c724d109b30cc908cd
dkchen <819873075@qq.com>
2017-07-26 21:41:03 +0800

    new

--- app/src/main/java/com/ckt/cc/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ckt/cc/beatbox/BeatBoxFragment.java
@@ -3,11 +3,14 @@ package com.ckt.cc.beatbox;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ckt.cc.beatbox.databinding.FragmentBeatBoxBinding;
+import com.ckt.cc.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * Created by Cc on 2017/7/26.
@@ -24,6 +27,20 @@ public class BeatBoxFragment extends Fragment {
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout
                 .fragment_beat_box, container, false);
 
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+
         return binding.getRoot();
     }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
 }
